30d6ea61dc8aca03a32a16f3178f85fbc99935da
H4rryp0tt3r <nagesh.podilapu@gmail.com>
2017-01-27 22:08:28 +0530

    Value types can also inherit/conform to protocols

--- ProtocolDrivenProgrammingPractice.playground/Contents.swift
+++ ProtocolDrivenProgrammingPractice.playground/Contents.swift
@@ -35,3 +35,31 @@ struct SwiftBird: Bird, Flyable {
         return version * 1000.0
     }
 }
+
+enum UnladenSwallow: Bird, Flyable {
+    case african
+    case european
+    case unknown
+
+    var name: String {
+        switch self {
+            case .african:
+                return "african"
+            case .european:
+                return "european"
+            case .unknown:
+                return "What do you mean? African or European?"
+        }
+    }
+
+    var airSpeedVelocity: Double {
+        switch self {
+            case .african:
+                return 10.0
+            case .european:
+                return 9.9
+            case .unknown:
+                return 0
+        }
+    }
+}
